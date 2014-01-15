0cac4b793b3eb7c122f1f6550f2e62ba29e02795
Joshua Prendergast <j.prendo10@gmail.com>
2014-01-15 23:09:09 +0000

    Handle exception straight away

--- src/main/uk/co/prenderj/trail/util/CheckedAsyncTask.java
+++ src/main/uk/co/prenderj/trail/util/CheckedAsyncTask.java
@@ -3,8 +3,6 @@ package uk.co.prenderj.trail.util;
 import android.os.AsyncTask;
 
 public abstract class CheckedAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
-    private Exception thrown;
-    
     public abstract Result call(Params... params) throws Exception;
     
     public abstract void finish(Result result) throws Exception;
@@ -14,16 +12,13 @@ public abstract class CheckedAsyncTask<Params, Progress, Result> extends AsyncTa
         try {
             return call(params);
         } catch (Exception e) {
-            thrown = e;
+            onException(e);
             return null;
         }
     }
     
     @Override
     protected void onPostExecute(Result result) {
-        if (thrown != null) {
-            onException(thrown);
-        }
         try {
             finish(result);
         } catch (Exception e) {
