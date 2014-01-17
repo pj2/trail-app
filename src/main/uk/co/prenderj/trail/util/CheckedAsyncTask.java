5f0deaeb864799e24209c0d08f28e88bcdba7a62
Joshua Prendergast <j.prendo10@gmail.com>
2014-01-17 00:39:04 +0000

    Revert "Handle exception straight away"
    This reverts commit 0cac4b793b3eb7c122f1f6550f2e62ba29e02795.

--- src/main/uk/co/prenderj/trail/util/CheckedAsyncTask.java
+++ src/main/uk/co/prenderj/trail/util/CheckedAsyncTask.java
@@ -3,6 +3,8 @@ package uk.co.prenderj.trail.util;
 import android.os.AsyncTask;
 
 public abstract class CheckedAsyncTask<Params, Progress, Result> extends AsyncTask<Params, Progress, Result> {
+    private Exception thrown;
+    
     public abstract Result call(Params... params) throws Exception;
     
     public abstract void finish(Result result) throws Exception;
@@ -12,13 +14,16 @@ public abstract class CheckedAsyncTask<Params, Progress, Result> extends AsyncTa
         try {
             return call(params);
         } catch (Exception e) {
-            onException(e);
+            thrown = e;
             return null;
         }
     }
     
     @Override
     protected void onPostExecute(Result result) {
+        if (thrown != null) {
+            onException(thrown);
+        }
         try {
             finish(result);
         } catch (Exception e) {
